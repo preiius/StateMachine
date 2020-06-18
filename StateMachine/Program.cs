e93d8b8d314e280959d1231c2cc413cf7813b659
jaz69 <jaz69@mail.com>
2020-06-19 02:31:01 +0800

    Initial commit

---
+++ StateMachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using BugTrackerExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // with Stateless
+
+            var bug = new Bug("Incorrect stock count");
+
+            bug.Assign("Joe");
+            bug.Defer();
+            bug.Assign("Harry");
+            bug.Assign("Fred");
+            bug.Close();
+
+            Console.WriteLine();
+            Console.WriteLine("State machine:");
+            Console.WriteLine(bug.ToDotGraph());
+
+
+            Console.ReadKey(false);
+        }
+    }
+}
