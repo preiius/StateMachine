7d9d9ed409afddc9e2b7d2713ceaa123c41aade9
jaz69 <jaz69@mail.com>
2020-06-19 02:57:57 +0800

    Remove unnecessary using in Program.cs

--- StateMachine/Program.cs
+++ StateMachine/Program.cs
@@ -1,9 +1,5 @@
-﻿using BugTrackerExample;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using BugTrackerExample;
 
 namespace StateMachine
 {
